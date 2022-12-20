35e6593c92aa5f16cc6e995b794a423afd3e64be
admin-gr00 <admin-gr00@AM-T-07405>
2022-12-20 10:49:34 +0100

    bool generuj i plansza

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -10,3 +10,14 @@ struct pole {
 
 };
 pole plansza[16][16];
+
+bool generuj_plansze()
+{
+	for (int x = 0; x<16; x++)
+		for (int y = 0; y < 16; y++)
+		{
+			plansza[x][y].wartosc = 0;
+			plansza[x][y].odkryte = false;
+		}
+	return true;
+}
